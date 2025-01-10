c7116ae00ed2f0f8fe8ddf8631e6d36b6d689bad
LukaszKretkowski <lukhenkre@gmail.com>
2025-01-10 11:33:19 +0100

    Dodanie kodu drugiego

--- zadanie17/plik.c
+++ zadanie17/plik.c
@@ -1,10 +1,14 @@
 #include <stdio.h>
+#include <math.h>
 
 float volume_cuboid(float x, float y, float h){
 return(x*y*h);
 }
+float volume_kula(float r){
+return(4.0/3.0) * M_PI * (r*r*r);
+}
 int main(){
-float x, y, h;
+float x, y, h, r;
 
 printf("Podaj dlugosc podstawy prostopadloscianu");
 scanf("%f", &x);
@@ -15,7 +19,11 @@ scanf("%f", &y);
 printf("Podaj wysokosc prostopadloscianu");
 scanf("%f", &h);
 
-printf ("Objêtosc prostopadloscianu: %f\n", volume_cuboid(x, y, h));
+printf("Podaj promien kuli");
+scanf("%f", &r);
+
+printf ("Objetosc prostopadloscianu: %f\n", volume_cuboid(x, y, h));
+printf ("Objetosc kuli: %f\n",volume_kula(r));
 
 return(0);
 
